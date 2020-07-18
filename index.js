509ec3f98f46f53a2e6305e73717eff2acfd9c20
Taz Hinkle <hinkle.taz@gmail.com>
2020-07-18 16:54:39 -0600

    First armies commit

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-var people = [
-    { name: 'bob', age: 33, death: 'tragic' },
-    { name: 'babs', age: 67, death: "you wouldn't believe it" },
-    { name: 'ignatious', age: 152, death: 'glorious' }
+var armies = [
+    { name: 'bob', archers: 33, mages: 12, melee: 20 },
+    { name: 'goats', archers: 33, mages: 22, melee: 20 },
 ];
-console.table(people);
+var fight = function(armyA, armyB) {
+    var armyAPostFight = { name: armyA.name };
+    var armyBPostFight = { name: armyB.name };
+    armyAPostFight.archers = armyA.archers - armyB.mages;
+    armyBPostFight.archers = armyB.archers - armyA.mages;
+    armyAPostFight.melee = armyA.melee - armyBPostFight.archers;
+    armyBPostFight.melee = armyB.melee - armyAPostFight.archers;
+    armyAPostFight.mages = armyA.mages - armyBPostFight.melee;
+    armyBPostFight.mages = armyB.mages - armyAPostFight.melee;
+    armyAPostFight.total = armyAPostFight.archers + armyAPostFight.mages + armyAPostFight.melee;
+    armyBPostFight.total = armyBPostFight.archers + armyBPostFight.mages + armyBPostFight.melee;
+    armyAPostFight.archers = Math.max(0, armyAPostFight.archers);
+    armyBPostFight.archers = Math.max(0, armyBPostFight.archers);
+    armyAPostFight.melee = Math.max(0, armyAPostFight.melee);
+    armyBPostFight.melee = Math.max(0, armyBPostFight.melee);
+    armyAPostFight.mages = Math.max(0, armyAPostFight.mages);
+    armyBPostFight.mages = Math.max(0, armyBPostFight.mages);
+    armyAPostFight.total = Math.max(0, armyAPostFight.total);
+    armyBPostFight.total = Math.max(0, armyBPostFight.total);
+    return {
+        armyA: armyAPostFight,
+        armyB: armyBPostFight,
+        winner: armyAPostFight.total > armyBPostFight.total
+            ? armyAPostFight.name
+            : armyAPostFight.total < armyBPostFight.total
+                ? armyBPostFight.name
+                : "Tie"
+    };
+}
+console.table(armies);
 var handleHTTPRequest = function(request, response) {
     var result = "<h1>what up, ma goats</h1>";
     console.log(request.url);
-    if (request.url === "/people") {
+    if (request.url === "/") {
         result = JSON.stringify(
-            people,
+            armies,
+            null,
+            "    "
+        );
+    } else if (request.url === "/fight") {
+        var fightResult = fight(armies[0], armies[1]);
+        armies[0] = fightResult.armyA;
+        armies[1] = fightResult.armyB;
+        result = JSON.stringify(
+            fightResult,
             null,
             "    "
         );
