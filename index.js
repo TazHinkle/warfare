6beea26fe9388ae820417b1981a1b880be9ab550
Taz Hinkle <hinkle.taz@gmail.com>
2020-08-08 16:00:10 -0600

    Checks both armies > 0 before fight

--- index.js
+++ index.js
@@ -8,31 +8,40 @@ function jsonClone(input) {
 }
 
 var fight = function(armyA, armyB) {
+    armyA.total = armyA.archers + armyA.mages + armyA.melee;
+    armyB.total = armyB.archers + armyB.mages + armyB.melee;
     var armyAPostFight = jsonClone(armyA);
     var armyBPostFight = jsonClone(armyB);
-    armyAPostFight.archers = Math.max(0, armyA.archers - armyB.mages);
-    armyBPostFight.archers = Math.max(0, armyB.archers - armyA.mages);
-    armyAPostFight.melee = Math.max(0, armyA.melee - armyBPostFight.archers);
-    armyBPostFight.melee = Math.max(0, armyB.melee - armyAPostFight.archers);
-    armyAPostFight.mages = Math.max(0, armyA.mages - armyBPostFight.melee);
-    armyBPostFight.mages = Math.max(0, armyB.mages - armyAPostFight.melee);
-    armyAPostFight.total = armyAPostFight.archers + armyAPostFight.mages + armyAPostFight.melee;
-    armyBPostFight.total = armyBPostFight.archers + armyBPostFight.mages + armyBPostFight.melee;
-    var winner;
-    if (armyAPostFight.total > armyBPostFight.total) {
-        winner = armyAPostFight.name;
-        armyAPostFight.wins += 1;
-    } else if(armyAPostFight.total < armyBPostFight.total) {
-        winner = armyBPostFight.name;
-        armyBPostFight.wins += 1;
-    } else {
-        winner = "Tie"
-    }      
+    var winner = "Cannot fight without an army";
+    if (armyA.total > 0 && armyB.total > 0) {
+        armyAPostFight.archers = Math.max(0, armyA.archers - armyB.mages);
+        armyBPostFight.archers = Math.max(0, armyB.archers - armyA.mages);
+        armyAPostFight.melee = Math.max(0, armyA.melee - armyBPostFight.archers);
+        armyBPostFight.melee = Math.max(0, armyB.melee - armyAPostFight.archers);
+        armyAPostFight.mages = Math.max(0, armyA.mages - armyBPostFight.melee);
+        armyBPostFight.mages = Math.max(0, armyB.mages - armyAPostFight.melee);
+        armyAPostFight.total = armyAPostFight.archers + armyAPostFight.mages + armyAPostFight.melee;
+        armyBPostFight.total = armyBPostFight.archers + armyBPostFight.mages + armyBPostFight.melee;
+        
+        if (armyAPostFight.total > armyBPostFight.total) {
+            winner = armyAPostFight.name;
+            armyAPostFight.wins += 1;
+        } else if(armyAPostFight.total < armyBPostFight.total) {
+            winner = armyBPostFight.name;
+            armyBPostFight.wins += 1;
+        } else {
+            winner = "Tie"
+        }      
+        
+            armyA = armyAPostFight;
+            armyB = armyBPostFight;
+            winner = winner;
+    }    
     return {
-        armyA: armyAPostFight,
-        armyB: armyBPostFight,
-        winner: winner
-    };
+        armyA,
+        armyB,
+        winner
+    }    
 }
 console.table(armies);
 var handleHTTPRequest = function(request, response) {
@@ -44,6 +53,8 @@ var handleHTTPRequest = function(request, response) {
             null,
             "    "
         );
+    }else if(request.url === "/login") {
+        //create login box that queries armies[] and selects the user based on input.
     }else if (request.url === "/fight") {
         var fightResult = fight(armies[0], armies[1]);
         armies[0] = fightResult.armyA;
@@ -59,6 +70,7 @@ var handleHTTPRequest = function(request, response) {
     response.end(result);
 };
 var http = require("http");
+const { fstat } = require("fs");
 var httpServer = http.createServer(handleHTTPRequest);
 var port = 3000;
 console.log("attempting to start an http server on port", port);
